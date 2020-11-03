02cb6c7bf076d511bd730815d62f5a3a70837151
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-11-03 13:36:16 -0500

    Create typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,16 @@
+const sentence = "Joshua Lerner";
+  for (i = 0; i < sentence.length-(sentence.length-1); i++) {
+  setTimeout(() => { process.stdout.write(sentence[0]);}, 1000)
+  setTimeout(() => { process.stdout.write(sentence[1]);}, 2000)
+  setTimeout(() => { process.stdout.write(sentence[2]);}, 3000)
+  setTimeout(() => { process.stdout.write(sentence[3]);}, 4000)
+  setTimeout(() => { process.stdout.write(sentence[4]);}, 5000)
+  setTimeout(() => { process.stdout.write(sentence[5]);}, 6000)
+  setTimeout(() => { process.stdout.write(sentence[6]);}, 7000)
+  setTimeout(() => { process.stdout.write(sentence[7]);}, 8000)
+  setTimeout(() => { process.stdout.write(sentence[8]);}, 9000)
+  setTimeout(() => { process.stdout.write(sentence[9]);}, 10000)
+  setTimeout(() => { process.stdout.write(sentence[10]);}, 11000)
+  setTimeout(() => { process.stdout.write(sentence[11]);}, 12000)
+  setTimeout(() => { process.stdout.write(sentence[12]);}, 13000)
+  }
